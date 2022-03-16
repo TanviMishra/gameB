38b0439229ad94852f43ae5df924fc63d2e2864f
Huaigu Lin <kevinlinhg@outlook.com>
2022-03-16 12:39:43 -0400

    add random movement

--- index.js
+++ index.js
@@ -2,6 +2,17 @@
 let shared;
 let ptShareds;
 let myShared;
+// previous move value, generated on each 10th frame
+let prevMoveVal0 = 0;
+let prevMoveVal1 = 0;
+let prevMoveVal2 = 0;
+let prevMoveVal3 = 0;
+let prevMoveVal4 = 0;
+let prevMoveVal5 = 0;
+let prevMoveVal6 = 0;
+let prevMoveVal7 = 0;
+let prevMoveVal8 = 0;
+let prevMoveVal9 = 0;
 
 function preload() {
   // connect & init shared variables
@@ -20,6 +31,8 @@ function setup() {
   if (partyIsHost()) {
     shared.sheepXY = [];
   }
+  // keeps record of which sheeps are in range of this dog
+  myShared.sheepsInRange = [];
   // init dog & sheep positions
   // 1st dog at top-left, 2nd at top-right...
   if (ptShareds.length % 4 === 1) {
@@ -38,6 +51,7 @@ function setup() {
   // 10 sheeps for each user at random positions
   for (let i = 0; i < 10; i++) {
     shared.sheepXY.push({
+      id: random(),
       x: (width - 60) * random() + 30,
       y: (height - 60) * random() + 30,
     });
@@ -80,7 +94,7 @@ function draw() {
   }
 
   fill(color(255, 255, 255));
-  shared.sheepXY.forEach((sheep) => {
+  shared.sheepXY.forEach((sheep, idx) => {
     // draw the sheeps
     circle(sheep.x, sheep.y, 20);
 
@@ -92,6 +106,9 @@ function draw() {
 
     if (dist(myShared.dogX, myShared.dogY, sheep.x, sheep.y) < 50) {
       // in stronger effect range
+      if (myShared.sheepsInRange.findIndex((id) => id === sheep.id) < 0) {
+        myShared.sheepsInRange.push(sheep.id);
+      }
       const speed = 2;
       // calculate speed x and y based on heading
       let speedX;
@@ -126,6 +143,9 @@ function draw() {
       }
     } else if (dist(myShared.dogX, myShared.dogY, sheep.x, sheep.y) < 100) {
       // in weaker effect range
+      if (myShared.sheepsInRange.findIndex((id) => id === sheep.id) < 0) {
+        myShared.sheepsInRange.push(sheep.id);
+      }
       const speed = 1;
       let speedX;
       let speedY;
@@ -155,6 +175,88 @@ function draw() {
         (sheep.y > width - 30 && myShared.dogY > sheep.y)
       ) {
         sheep.y += speedY;
+      }
+    } else {
+      const foundIndex = myShared.sheepsInRange.findIndex(
+        (id) => id === sheep.id
+      );
+      if (foundIndex >= 0) {
+        myShared.sheepsInRange.splice(foundIndex, 1);
+      }
+      let inOtherDogsRange = false;
+      ptShareds.forEach((dog) => {
+        if (dog.sheepsInRange.findIndex((id) => id === sheep.id) >= 0) {
+          inOtherDogsRange = true;
+        }
+      });
+      // not in effect range, move randomly
+      if (partyIsHost() && !inOtherDogsRange) {
+        if (frameCount % 50 === 0) {
+          prevMoveVal0 = Math.random() * (Math.round(Math.random()) ? 1 : -1);
+          prevMoveVal1 = Math.random() * (Math.round(Math.random()) ? 1 : -1);
+          prevMoveVal2 = Math.random() * (Math.round(Math.random()) ? 1 : -1);
+          prevMoveVal3 = Math.random() * (Math.round(Math.random()) ? 1 : -1);
+          prevMoveVal4 = Math.random() * (Math.round(Math.random()) ? 1 : -1);
+          prevMoveVal5 = Math.random() * (Math.round(Math.random()) ? 1 : -1);
+          prevMoveVal6 = Math.random() * (Math.round(Math.random()) ? 1 : -1);
+          prevMoveVal7 = Math.random() * (Math.round(Math.random()) ? 1 : -1);
+          prevMoveVal8 = Math.random() * (Math.round(Math.random()) ? 1 : -1);
+          prevMoveVal9 = Math.random() * (Math.round(Math.random()) ? 1 : -1);
+        }
+        if (sheep.x < width - 30 && sheep.x > 30 && frameCount % 50 < 50) {
+          if (idx % 10 === 0) {
+            sheep.x += prevMoveVal0;
+          } else if (idx % 10 === 1) {
+            sheep.x += prevMoveVal1;
+          } else if (idx % 10 === 2) {
+            sheep.x += prevMoveVal2;
+          } else if (idx % 10 === 3) {
+            sheep.x += prevMoveVal3;
+          } else if (idx % 10 === 4) {
+            sheep.x += prevMoveVal4;
+          } else if (idx % 10 === 5) {
+            sheep.x += prevMoveVal5;
+          } else if (idx % 10 === 6) {
+            sheep.x += prevMoveVal6;
+          } else if (idx % 10 === 7) {
+            sheep.x += prevMoveVal7;
+          } else if (idx % 10 === 8) {
+            sheep.x += prevMoveVal8;
+          } else if (idx % 10 === 9) {
+            sheep.x += prevMoveVal9;
+          }
+        } else if (sheep.x < 30) {
+          sheep.x += Math.random();
+        } else {
+          sheep.x -= Math.random();
+        }
+        if (sheep.y < height - 30 && sheep.y > 30 && frameCount % 50 < 50) {
+          if (idx % 10 === 0) {
+            sheep.y += prevMoveVal0 * (Math.round(Math.random()) ? 1 : -1);
+          } else if (idx % 10 === 1) {
+            sheep.y += prevMoveVal1 * (Math.round(Math.random()) ? 1 : -1);
+          } else if (idx % 10 === 2) {
+            sheep.y += prevMoveVal2 * (Math.round(Math.random()) ? 1 : -1);
+          } else if (idx % 10 === 3) {
+            sheep.y += prevMoveVal3 * (Math.round(Math.random()) ? 1 : -1);
+          } else if (idx % 10 === 4) {
+            sheep.y += prevMoveVal4 * (Math.round(Math.random()) ? 1 : -1);
+          } else if (idx % 10 === 5) {
+            sheep.y += prevMoveVal5 * (Math.round(Math.random()) ? 1 : -1);
+          } else if (idx % 10 === 6) {
+            sheep.y += prevMoveVal6 * (Math.round(Math.random()) ? 1 : -1);
+          } else if (idx % 10 === 7) {
+            sheep.y += prevMoveVal7 * (Math.round(Math.random()) ? 1 : -1);
+          } else if (idx % 10 === 8) {
+            sheep.y += prevMoveVal8 * (Math.round(Math.random()) ? 1 : -1);
+          } else if (idx % 10 === 9) {
+            sheep.y += prevMoveVal9 * (Math.round(Math.random()) ? 1 : -1);
+          }
+        } else if (sheep.y < 30) {
+          sheep.y += Math.random();
+        } else {
+          sheep.y -= Math.random();
+        }
       }
     }
   });
