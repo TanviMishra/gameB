4c6c4f94e92c40f8b738ea0ac8f90575d3a9c5df
TanviMishra <misht490@newschool.edu>
2022-03-26 19:58:59 -0400

    fence boundaries updated

--- index.js
+++ index.js
@@ -14,11 +14,18 @@ let prevMoveVal7 = 0;
 let prevMoveVal8 = 0;
 let prevMoveVal9 = 0;
 
+let hit1 = false; let hit2 = false; let hit3 = false;
+let col="#1b5bc2";
+let dogRadius=20;
+let sheepRadius=20;
+let dogFenceHit = false;
+let sheepFenceHit = false;
+
 function preload() {
   // connect & init shared variables
   partyConnect(
     "wss://deepstream-server-1.herokuapp.com",
-    "sheep_movement",
+    "sheep_movement_tm",
     "main"
   );
   shared = partyLoadShared("shared");
@@ -28,6 +35,10 @@ function preload() {
 
 function setup() {
   createCanvas(800, 800);
+  fenceWidth = 8;
+  fenceHeight = 80;
+  fenceStartX = 700;
+  fenceStartY = 700;
   if (partyIsHost()) {
     shared.sheepXY = [];
   }
@@ -62,12 +73,11 @@ function setup() {
 
 function draw() {
   background(220);
-
   // draw dogs
   ptShareds.forEach((partcpt, idx) => {
     noStroke();
     fill(color(138, 48, 0));
-    circle(partcpt.dogX, partcpt.dogY, 20);
+    circle(partcpt.dogX, partcpt.dogY, dogRadius);
     // stronger effect range
     fill(color(138, 48, 0, 100));
     circle(partcpt.dogX, partcpt.dogY, 100);
@@ -75,60 +85,98 @@ function draw() {
     fill(color(138, 48, 0, 50));
     circle(partcpt.dogX, partcpt.dogY, 200);
   });
-
+  dogFenceHit = checkFenceHit(myShared.dogX, myShared.dogY,dogRadius) //check if dog is hitting the fence
   // detect key presses
   if (keyIsPressed) {
-    if (keyIsDown(LEFT_ARROW)) {
-      if (myShared.dogX > 5) myShared.dogX -= 5;
-      if (keyIsDown(DOWN_ARROW)) {
-        if (myShared.dogY < height - 5) myShared.dogY += 5;
-      } else if (keyIsDown(UP_ARROW)) {
-        if (myShared.dogY > 5) myShared.dogY -= 5;
-      }
-    } else if (keyIsDown(RIGHT_ARROW)) {
-      if (myShared.dogX < width - 5) myShared.dogX += 5;
-      if (keyIsDown(DOWN_ARROW)) {
-        if (myShared.dogY < height - 5) myShared.dogY += 5;
-      } else if (keyIsDown(UP_ARROW)) {
-        if (myShared.dogY > 5) myShared.dogY -= 5;
-      }
-    } else if (keyIsDown(UP_ARROW)) {
-      if (myShared.dogY > 5) myShared.dogY -= 5;
-      if (keyIsDown(LEFT_ARROW)) {
+    if(dogFenceHit){
+      myShared.dogX=prevDogX; myShared.dogY=prevDogY; //fence hit so send dog back to previous position
+    }
+    else{
+      prevDogX=myShared.dogX; prevDogY=myShared.dogY; //update previous position
+      if(keyIsDown(LEFT_ARROW)) {
         if (myShared.dogX > 5) myShared.dogX -= 5;
-      } else if (keyIsDown(RIGHT_ARROW)) {
+        if (keyIsDown(DOWN_ARROW)) {
+          if (myShared.dogY < height - 5) myShared.dogY += 5;
+        } else if (keyIsDown(UP_ARROW)) {
+          if (myShared.dogY > 5) myShared.dogY -= 5;
+        }
+      } 
+      else if (keyIsDown(RIGHT_ARROW)) {
         if (myShared.dogX < width - 5) myShared.dogX += 5;
-      }
-    } else if (keyIsDown(DOWN_ARROW)) {
-      if (myShared.dogY < height - 5) myShared.dogY += 5;
-      if (keyIsDown(LEFT_ARROW)) {
-        if (myShared.dogX > 5) myShared.dogX -= 5;
-      } else if (keyIsDown(RIGHT_ARROW)) {
-        if (myShared.dogX < width - 5) myShared.dogX += 5;
+        if (keyIsDown(DOWN_ARROW)) {
+          if (myShared.dogY < height - 5) myShared.dogY += 5;
+        } else if (keyIsDown(UP_ARROW)) {
+          if (myShared.dogY > 5) myShared.dogY -= 5;
+        }
+      } 
+      else if (keyIsDown(UP_ARROW)) {
+        if (myShared.dogY > 5) myShared.dogY -= 5;
+        if (keyIsDown(LEFT_ARROW)) {
+          if (myShared.dogX > 5) myShared.dogX -= 5;
+        } else if (keyIsDown(RIGHT_ARROW)) {
+          if (myShared.dogX < width - 5) myShared.dogX += 5;
+        }
+      } 
+      else if (keyIsDown(DOWN_ARROW)) {
+        if (myShared.dogY < height - 5) myShared.dogY += 5;
+        if (keyIsDown(LEFT_ARROW)) {
+          if (myShared.dogX > 5) myShared.dogX -= 5;
+        } else if (keyIsDown(RIGHT_ARROW)) {
+          if (myShared.dogX < width - 5) myShared.dogX += 5;
+        }
       }
     }
   }
-
   fill(color(255, 255, 255));
   shared.sheepXY.forEach((sheep, idx) => {
     // draw the sheeps
-    circle(sheep.x, sheep.y, 20);
-
+    circle(sheep.x, sheep.y, sheepRadius);
+    sheepFenceHit = checkFenceHit(sheep.x, sheep.y, sheepRadius) //check if sheep is hitting the fence
     // calculate heading
     const heading = createVector(
       sheep.x - myShared.dogX,
       sheep.y - myShared.dogY
     ).heading();
-
-    if (dist(myShared.dogX, myShared.dogY, sheep.x, sheep.y) < 50) {
+    if(sheepFenceHit){
+      let speed = -1; console.log('hit')
+      let speedX; let speedY;
+      if (heading >= 0 && heading < 90) {
+        speedX = sin(heading) * speed;
+        speedY = cos(heading) * speed;
+      } else if (heading >= 90) {
+        speedX = -sin(180 - heading) * speed;
+        speedY = cos(180 - heading) * speed;
+      } else if (heading < 0 && heading >= -90) {
+        speedX = sin(-heading) * speed;
+        speedY = -cos(-heading) * speed;
+      } else {
+        speedX = -sin(180 + heading) * speed;
+        speedY = -cos(180 + heading) * speed;
+      }
+      // add speed x and y to sheep while preventing them from going out of canvas
+      if (
+        (sheep.x < width - 30 && sheep.x > 30) ||
+        (sheep.x < 30 && myShared.dogX < sheep.x) ||
+        (sheep.x > width - 30 && myShared.dogX > sheep.x)
+      ) {
+        sheep.x += speedX;
+      }
+      if (
+        (sheep.y < height - 30 && sheep.y > 30) ||
+        (sheep.y < 30 && myShared.dogY < sheep.y) ||
+        (sheep.y > width - 30 && myShared.dogY > sheep.y)
+      ) {
+        sheep.y += speedY;
+      }
+    } else if (dist(myShared.dogX, myShared.dogY, sheep.x, sheep.y) < 50) {
       // in stronger effect range
       if (myShared.sheepsInRange.findIndex((id) => id === sheep.id) < 0) {
         myShared.sheepsInRange.push(sheep.id);
       }
-      const speed = 2;
+      if(sheepFenceHit){speed = -1; console.log('hit')}
+      else speed = 2;
       // calculate speed x and y based on heading
-      let speedX;
-      let speedY;
+      let speedX; let speedY;
       if (heading >= 0 && heading < 90) {
         speedX = sin(heading) * speed;
         speedY = cos(heading) * speed;
@@ -162,9 +210,9 @@ function draw() {
       if (myShared.sheepsInRange.findIndex((id) => id === sheep.id) < 0) {
         myShared.sheepsInRange.push(sheep.id);
       }
-      const speed = 1;
-      let speedX;
-      let speedY;
+      if(sheepFenceHit){speed = -1; console.log('hit')}
+      else speed = 1;
+      speedX= speed; speedY= speed;
       if (heading >= 0 && heading < 90) {
         speedX = sin(heading) * speed;
         speedY = cos(heading) * speed;
@@ -276,4 +324,25 @@ function draw() {
       }
     }
   });
+  drawFence();
+}
+function drawFence(){
+  fill(col);
+  rect(fenceStartX,fenceStartY,fenceWidth,fenceHeight);
+  rect(fenceStartX,fenceStartY+fenceHeight-fenceWidth,fenceHeight,fenceWidth);
+  rect(fenceStartX+fenceHeight-fenceWidth,fenceStartY,fenceWidth,fenceHeight);
+}
+function checkFenceHit(objX,objY,r){
+  hit1 = collideRectCircle(fenceStartX,fenceStartY,fenceWidth,fenceHeight,objX,objY,r);
+  hit2 = collideRectCircle(fenceStartX,fenceStartY+fenceHeight-fenceWidth,fenceHeight,fenceWidth,objX,objY,r);
+  hit3 = collideRectCircle(fenceStartX+fenceHeight-fenceWidth,fenceStartY,fenceWidth,fenceHeight,objX,objY,r);
+  if(hit1 || hit2 || hit3){
+    return true;
+  }
+  else{
+    return false;
+  }
+}
+function keyMove(){
+
 }
